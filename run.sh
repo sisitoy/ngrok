7fc41cab9287a7fed6791c224577950672b0f273
sisitoy <83447531+sisitoy@users.noreply.github.com>
2021-05-04 08:46:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
 echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 
-get_ipython().system_raw('/usr/sbin/sshd -D &')
+/usr/sbin/sshd -D
 # to check if ssh is running on port 22 or not or installed or not
 sudo apt install net-tools
 netstat -plant | grep ssh
@@ -27,11 +27,3 @@ which sshd
 
 /usr/bin/sshd 
 tcp        0      0 0.0.0.0:22              0.0.0.0:*               LISTEN 1008/sshd
-
-# get_ipython().system_raw('./ngrok authtoken $authtoken && ./ngrok tcp 22 &')
-with urllib.request.urlopen('http://localhost:4040/api/tunnels') as response:
-  data = json.loads(response.read().decode())
-  (host, port) = data['tunnels'][0]['public_url'][6:].split(':')
-  print(f'SSH command: ssh -p{port} root@{host}')
-#Print root password
-print(f'SSH password: {password}')
